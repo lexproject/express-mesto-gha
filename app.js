8eac36378ccd1535edee69619e9d1f48443bbf79
Victor Ojog <infonetroom@yandex.ru>
2022-09-06 17:45:15 +0300

    test commit 3 error 404

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
 });
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+app.use((req, res)=>res.status(404).send({message:'Запрос сформирован неверно. Ресурс не найден'}));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
